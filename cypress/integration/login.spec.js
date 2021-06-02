102d8b6227a6650b7848f80cc962ea1f038a3f8c
Fernando Barrios <fer.b.1990@gmail.com>
2021-06-01 20:23:43 -0600

    add login test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,15 +1,30 @@
-context("Pruebas de Login", () => {
-    it("Crear una cuenta", () => {
-      cy.visit("http://thedemosite.co.uk/addauser.php");
-      cy.get("input[name='username']").type("telusqatest");
-      cy.get("input[type='password']").type("telusqatest");
-      cy.get("input[name='FormsButton2']").click();
-      cy.get('form a[href="login.php"]').click();
-      cy.location('pathname').should('contain', 'login.php');
-      cy.get("input[name='username']").type("telusqatest");
-      cy.get("input[type='password']").type("telusqatest");
-      cy.get("input[name='FormsButton2']").click();
-      cy.get(".auto-style1 big blockquote > blockquote b").should('have.text', '**Successful Login**');
-    });
+/// <reference types="cypress" />
+
+context("Pruebas de login", () => {
+  it("Probar login satisfactorio", () => {
+    // visitar la URL para crear usuario
+    cy.visit("http://thedemosite.co.uk/addauser.php");
+
+    // Rellenar formulario de añadir usuario
+    cy.get('input[name="username"]').type("telusprueba1", { delay: 500 });
+    cy.get('input[name="username"]')
+      .type("{selectall}{del}")
+      .type("telusprueba1")
+      .should("have.value", "telusprueba1");
+    cy.get('input[name="password"]').type("telusprueba1");
+    cy.get('input[name="FormsButton2"]').click();
+
+    // Visitar la pagina para probar el usuario y contraseña
+    cy.get('form a[href="login.php"]').click();
+    cy.location("pathname").should("contains", "login.php");
+
+    // Rellenar formulario para inicio de sesión
+    cy.get('input[name="username"]').type("telusprueba1");
+    cy.get('input[name="password"]').type("telusprueba1");
+    cy.get('input[name="FormsButton2"]').click();
+    cy.get("table big blockquote > blockquote b").should(
+      "have.text",
+      "**Successful Login**"
+    );
   });
-  
+});
