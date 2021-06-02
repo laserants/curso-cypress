e41db9994b6895c545b62db6a4de83a805a164d5
Fernando Barrios <fer.b.1990@gmail.com>
2021-06-02 06:54:29 -0600

    refactor folders

--- cypress/integration/calculadora.spec.js
+++ cypress/integration/calculadora.spec.js
@@ -1,11 +1,13 @@
+/// <reference types="cypress" />
+
 context("Pruebas e2e calculadora", () => {
   it("Suma", () => {
     cy.visit("http://127.0.0.1:8080/calculadora.html");
-    cy.get('.key-5').click();
-    cy.get('.key-add').click();
-    cy.get('.key-8').click();
-    cy.get('.key-equals').click();
-    cy.get('.key-equals').click();
-    cy.get('.auto-scaling-text').should('have.text', '13');
+    cy.get(".key-5").click();
+    cy.get(".key-add").click();
+    cy.get(".key-8").click();
+    cy.get(".key-equals").click();
+    cy.get(".key-equals").click();
+    cy.get(".auto-scaling-text").should("have.text", "13");
   });
 });
